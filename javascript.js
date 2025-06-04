7cf5dbb343a785fc5a3daf7f92b3df0c005df9a7
luukdg <luukdegraaf1993@gmail.com>
2025-06-04 10:38:54 +0200

    working grid changer

--- javascript.js
+++ javascript.js
@@ -1,33 +1,51 @@
-// Creating a grid with div's
-const container = document.querySelector(".container");
-for (let i = 1; i < 257; i++) {
+function gridMaker (gridAnswer) {
+    const container = document.querySelector(".container");
+    let gridSize = gridAnswer * gridAnswer
+
+    // Remove all content for container
+    container.innerHTML = '';
+
+    for (let i = 0; i < gridSize; i++) {
         const squareDiv = document.createElement("div");
         squareDiv.classList.add("squareDiv");
+        squareDiv.style.width = `calc(100% / ${gridAnswer})`;
+        squareDiv.style.height = `calc(100% / ${gridAnswer})`;
         container.appendChild(squareDiv);
     }
+}
 
 // Hover color changing function
-const hover = document.querySelectorAll(".squareDiv");
-hover.forEach((div) => {
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "red";
-    });
+function applyHoverColor () {
+    const hover = document.querySelectorAll(".squareDiv");
+    hover.forEach((div) => {
+        div.addEventListener("mouseover", () => {
+            if (div.style.backgroundColor) {
+                return;
+            } else {
+                let randomColor = Math.floor(Math.random()*16777215).toString(16);
+                let rgbValue = "#" + randomColor
+                div.style.backgroundColor = rgbValue;
+                div.style.opacity = 1;
+            }
+        });
 });
+}
 
 // Button grid changing
 const button = document.querySelector(".gridButton");
 button.addEventListener("click", () => {
+    // Prompt for an answer
     let gridAnswer = prompt("Enter a number below 100");
-    let gridSize = gridAnswer * gridAnswer
 
-    // remove div
-    document.querySelectorAll(".squareDiv").forEach(div => div.remove());
-
-    // create new div's
-    const container = document.querySelector(".container");
-    for (let i = 1; i < gridSize; i++) {
-        const squareDiv = document.createElement("div");
-        squareDiv.classList.add("squareDiv");
-        container.appendChild(squareDiv);
+    if (gridAnswer >= 100 || gridAnswer <= 0 || isNaN(gridAnswer)) {
+        alert("Please enter a valid number between 1 and 99.")
+    }
+    else {
+        gridMaker (gridAnswer);
+        applyHoverColor ();
     }
 });
+
+// Starting grid and hover color
+gridMaker (16);
+applyHoverColor ();
